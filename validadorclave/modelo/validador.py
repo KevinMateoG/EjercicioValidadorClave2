72f2ec786511fe0b6c1761d35ac8726b330069c5
Kevin Mateo G <gomezdiezkevinmateo@gmail.com>
2024-10-17 11:37:23 -0500

    Creacion de la clase ReglaValidacionCalisto y sus funciones

--- validadorclave/modelo/validador.py
+++ validadorclave/modelo/validador.py
@@ -2,7 +2,7 @@
 from abc import ABC, abstractmethod
 
 from validadorclave.modelo.errores import NoCumpleLongitudMinimaError, NoTieneLetraMayusculaError, \
-    NoTieneLetraMinusculaError, NoTieneNumeroError
+    NoTieneLetraMinusculaError, NoTieneNumeroError, NoTieneCaracterEspecialError
 
 
 class ReglaValidacion(ABC):
@@ -17,24 +17,39 @@ class ReglaValidacion(ABC):
     def _validar_longitud(self, clave: str) -> bool:
         longitud_clave = len(clave)
         if not longitud_clave >= self._longitud_esperada:
-            raise NoCumpleLongitudMinimaError
+            raise NoCumpleLongitudMinimaError()
         return True
 
     def _contiene_mayuscula(self, clave: str) -> bool:
         if not clave.isupper():
-            raise NoTieneLetraMayusculaError
+            raise NoTieneLetraMayusculaError()
         return True
 
     def _contiene_minuscula(self, clave: str) -> bool:
         if not clave.islower():
-            raise NoTieneLetraMinusculaError
+            raise NoTieneLetraMinusculaError()
         return True
 
     def _contiene_numero(self, clave: str) -> bool:
         if not clave.isdigit():
-            raise NoTieneNumeroError
+            raise NoTieneNumeroError()
         return True
 
+
+
+
+class ReglaValidacionGanimedes(ReglaValidacion):
+    def contiene_caracter_especial(self, clave: str):
+        if not "@" or "_" or "#" or "$" or "%" in clave:
+            raise NoTieneCaracterEspecialError()
+        return True
+
+
+class ReglaValidacionCalisto(ReglaValidacion):
+    def contiene_calisto(self, clave: str) -> bool:
+        calisto={"A": "a",
+                 "B": "b",
+                 }
 
 class Validador(ReglaValidacion):
     def __init__(self, regla: ReglaValidacion, _longitud_esperada: int):
@@ -43,12 +58,3 @@ class Validador(ReglaValidacion):
 
     def es_valida(self, clave: str) -> bool:
         pass
-
-class ReglaValidacionGanimedes(ReglaValidacion):
-
-    def contiene_caracter_especial(self, clave: str):
-        if not "@" or "_" or "#" or "$" or "%" in clave:
-            pass
-
-class ReglaValidacionCalisto(ReglaValidacion):
-    pass
