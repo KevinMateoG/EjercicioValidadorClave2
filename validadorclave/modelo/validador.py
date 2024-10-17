4549acb410ca3b785abb5cac6e21a976d1b21e51
Kevin Mateo G <gomezdiezkevinmateo@gmail.com>
2024-10-17 10:22:43 -0500

    Creacion de la clase ReglaValidacion, funciones es_valida y _valida_longitud

--- validadorclave/modelo/validador.py
+++ validadorclave/modelo/validador.py
@@ -1,1 +1,17 @@
 # TODO: Implementa el código del ejercicio aquí
+from abc import ABC, abstractmethod
+
+from validadorclave.modelo.errores import NoCumpleLongitudMinimaError
+
+
+class ReglaValidacion(ABC):
+    def __init__(self, _longitud_esperada: int):
+        self._longitud_esperada: int = _longitud_esperada
+
+    @abstractmethod
+    def es_valida(self):
+        ...
+
+    def _validar_longitud(self):
+        if not self._longitud_esperada >= 8:
+            raise NoCumpleLongitudMinimaError("No cumples con la longitud")
