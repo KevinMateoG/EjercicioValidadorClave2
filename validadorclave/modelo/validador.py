08c13bace7d5e6e211d4612989a09476a44b0998
Kevin Mateo G <gomezdiezkevinmateo@gmail.com>
2024-10-17 11:24:06 -0500

    Creacion fucionamiento de la clase ReglaValidacionGanimedes

--- validadorclave/modelo/validador.py
+++ validadorclave/modelo/validador.py
@@ -11,7 +11,7 @@ class ReglaValidacion(ABC):
         self._longitud_esperada: int = _longitud_esperada
 
     @abstractmethod
-    def es_valida(self):
+    def es_valida(self, clave: str):
         ...
 
     def _validar_longitud(self, clave: str) -> bool:
@@ -25,12 +25,30 @@ class ReglaValidacion(ABC):
             raise NoTieneLetraMayusculaError
         return True
 
-    def _contiene_minuscula(self, clave: str):
+    def _contiene_minuscula(self, clave: str) -> bool:
         if not clave.islower():
             raise NoTieneLetraMinusculaError
         return True
-    def _contiene_numero(self, clave: str):
+
+    def _contiene_numero(self, clave: str) -> bool:
         if not clave.isdigit():
             raise NoTieneNumeroError
         return True
 
+
+class Validador(ReglaValidacion):
+    def __init__(self, regla: ReglaValidacion, _longitud_esperada: int):
+        super().__init__(_longitud_esperada)
+        self.regla: ReglaValidacion = regla
+
+    def es_valida(self, clave: str) -> bool:
+        pass
+
+class ReglaValidacionGanimedes(ReglaValidacion):
+
+    def contiene_caracter_especial(self, clave: str):
+        if not "@" or "_" or "#" or "$" or "%" in clave:
+            pass
+
+class ReglaValidacionCalisto(ReglaValidacion):
+    pass
