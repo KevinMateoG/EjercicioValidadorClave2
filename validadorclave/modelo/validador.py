260eb7f2815e4f475e509cfa7d9c3f55a5d9611e
Kevin Mateo G <gomezdiezkevinmateo@gmail.com>
2024-10-17 10:43:29 -0500

    Arreglo de la funcion _validar_longitud

--- validadorclave/modelo/validador.py
+++ validadorclave/modelo/validador.py
@@ -5,6 +5,7 @@ from validadorclave.modelo.errores import NoCumpleLongitudMinimaError
 
 
 class ReglaValidacion(ABC):
+
     def __init__(self, _longitud_esperada: int):
         self._longitud_esperada: int = _longitud_esperada
 
@@ -12,6 +13,17 @@ class ReglaValidacion(ABC):
     def es_valida(self):
         ...
 
-    def _validar_longitud(self):
-        if not self._longitud_esperada >= 8:
+    def _validar_longitud(self, clave: str) -> bool:
+        longitud = len(clave)
+        if not longitud >= 8:
             raise NoCumpleLongitudMinimaError("No cumples con la longitud")
+        return True
+
+    def _contiene_mayuscula(self):
+        pass
+    def _contiene_minuscula(self):
+        pass
+
+    def _contiene_numero(self):
+        ...
+
